f6239631de0d44fed0f3fc390c45b13f1210b1cd
Sommerregen <sommerregen@users.noreply.github.com>
2015-05-12 21:17:40 +0200

    :maple_leaf: Version update - Release v1.0.2

--- smileys.php
+++ smileys.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Smileys v1.0.1
+ * Smileys v1.0.2
  *
  * This plugin substitutes text emoticons, also known as smilies
  * like :-), with images.
@@ -8,7 +8,7 @@
  * Licensed under MIT, see LICENSE.
  *
  * @package     Smileys
- * @version     1.0.1
+ * @version     1.0.2
  * @link        <https://github.com/sommerregen/grav-plugin-smileys>
  * @author      Benjamin Regler <sommerregen@benjamin-regler.de>
  * @copyright   2015, Benjamin Regler
